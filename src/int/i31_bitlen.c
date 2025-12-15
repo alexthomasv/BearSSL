6b0b0d47c4f7adba7f1b3fd8234e8696d757caf3
Alex Thomas <alexthomas@berkeley.edu>
2025-12-15 08:40:13 +0000

    Add bit len invariant

--- src/int/i31_bitlen.c
+++ src/int/i31_bitlen.c
@@ -23,22 +23,142 @@
  */
 
 #include "inner.h"
+#include "g_header.h"
 
 /* see inner.h */
 uint32_t
 br_i31_bit_length(uint32_t *x, size_t xlen)
 {
+    size_t xlen_init = xlen;
 	uint32_t tw, twk;
-
+	size_t C = __VERIFIER_nondet_size_t();
 	tw = 0;
 	twk = 0;
-	while (xlen -- > 0) {
-		uint32_t w, c;
 
-		c = EQ(tw, 0);
-		w = x[xlen];
-		tw = MUX(c, w, tw);
-		twk = MUX(c, (uint32_t)xlen, twk);
+	while (xlen > 0) {
+        __SMACK_code(
+            "assume {:loop_invariant} {:custom \"br_i31_bit_length\"} {:free_var @} {:x @} {:xlen @} {:twk @} {:tw @} {:mem_map MEM(@)} "
+            "$and.i1("
+                // 1. Witness Bounds (C < xlen_init)
+                "$ult.i64(@, @), " 
+        
+                "$and.i1("
+                    // -------------------------------------------------------
+                    // 2. Witness Property: x[C] != 0
+                    // Manual Load: load(MEM(x), x + C*4)
+                    "$ne.i32("
+                        "$load.i32(MEM(@), $add.i64(@, $mul.i64(@, 4))), " 
+                        "0"
+                    "), "
+        
+                    "$and.i1("
+                        // 3. Implication: (xlen <= C) ==> (twk == C)
+                        "$or.i1("
+                            "$ugt.i64(@, @), "          
+                            "$eq.i32(@, $trunc.i64.i32(@))" 
+                        "), "
+        
+                        "$and.i1("
+                            // -----------------------------------------------
+                            // 4. Implication: (xlen <= C) ==> (tw == x[C])
+                            // Manual Load: val(MEM(x), x + C*4)
+                            "$or.i1("
+                                "$ugt.i64(@, @), "      
+                                "$eq.i32(@, "           
+                                    "$load.i32(MEM(@), $add.i64(@, $mul.i64(@, 4)))"
+                                ")"
+                            "), "
+        
+                            // 5. Implication: (xlen > C) ==> (twk == xlen)
+                            "$or.i1("
+                                "$ule.i64(@, @), "          
+                                "$eq.i32(@, $trunc.i64.i32(@))" 
+                            ")"
+                        ")"
+                    ")"
+                ")"
+            ") == 1;",
+            // --- Argument Mapping ---
+            // 0. Free Variable (C)
+            C, x, xlen, twk, tw, x, 
+            // 1. Witness Bounds
+            C, xlen_init,
+            // 2. Witness Non-Zero: Needs (x, x, C)
+            x, x, C, 
+            // 3. Post-Latch Index
+            xlen, C, twk, C,
+            // 4. Post-Latch Value: Needs (xlen, C, tw) AND (x, x, C)
+            xlen, C, tw, x, x, C,
+            // 5. Pre-Latch Index
+            xlen, C, twk, xlen
+        );
+		xlen--;
+        uint32_t c = EQ(tw, 0);
+		tw = MUX(c, x[xlen], tw);
+
+        // Preconditions -> is this enough to prove the below target assert?
+        // (xlen <= C) ==> (twk == C)
+        // (xlen <= C) ==> (tw == x[C])
+        // (xlen >  C) ==> (twk == xlen)
+        // (xlen >  C) ==> (x[xlen] == 0)
+        // (C >= 0) /\ (C < xlen)
+        // x[C] != 0
+        // xlen in {0, 1 .. 16, 17, 18, 19, 20}
+        // twk_old in {20, 19, 18, 17, 16}
+        // x[16] == 0x1834
+
+        twk = MUX(c, (uint32_t)xlen, twk);
 	}
+    // Preconditions: 
+    // twk in {20, 19, 18, 17, 16}
+    // xlen_init == 21
+    // xlen <= 0
+    // xlen in {0, 1 .. 16, 17, 18, 19, 20}
+    // (xlen <= C) ==> (twk == C)
+    // (xlen <= C) ==> (tw == x[C])
+    // (xlen >  C) ==> (twk == xlen)
+    // (xlen >  C) ==> (x[xlen] == 0)
+    // (C >= 0) /\ (C < xlen_init)
+    // x[C] != 0
+    // x[16] != 0 /\ x[17] == 0 /\ x[18] == 0 /\ x[19] == 0 /\ x[20] == 0
+
+    // assert(twk == 16); // <- I want to prove this
+
+
+        // My facts:
+        // xlen <= C => twk == C /\ tw == x[C]
+        // xlen > C => twk == xlen /\ x[xlen] == 0
+        // x[C] != 0 /\ 0 <= C < xlen
+        // (xlen < C) ==> (c == 0)
+        // c == tw == 0
+ 
+        // tw in {0x1934, 0}
+        // c in {0, 1}
+        // xlen in [0, 1 .. 16] 
+        // twk_old in {20, 19, 18, 17, 16} 
+        // xlen in (0, 1 .. 17, 18 19 20)
+
+        // tw == 0 
+
+        // WHAT I WANT TO PROVE: 
+        // twk_new in {20, 19, 18, 17, 16}
+        // twk_new = twk_old if (tw != 0) else twk_new = xlen
+
+    // requires: twk in {20, 18, 17, 16} /\  xlen in (0, 1 .. 17, 18 19 20}
+	// assert(twk == 16)
+    // twk == C
+    
+    // xlen <= 0 
+    // xlen <= C => twk == C /\   tw == x[C]
+    // xlen > C => twk == xlen /\ x[xlen] == 0
+    // x[C] != 0 /\ 0 <= C < xlen
+
+    // xlen in (0, 1 .. 17, 18 19 20)
+    // tw = 0x1934, 0
+    // twk in (20, 18, 17, 16)
+
+    // twk == 16 <- the target i want to prove
+
+    
 	return (twk << 5) + BIT_LENGTH(tw);
 }
